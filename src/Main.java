45c1c96ac75df3dbd82012f57c6e6a55ff3654db
熊健文 <1290922719@qq.com>
2025-02-04 22:11:57 +0800

    我的java补课7
    * File类

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,94 @@ import Learn.Dog;
 import Learn.IRun;
 import Learn.Manager;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        byte[] buffer = new byte[1024];
+        int sum = 0;
 
-        final String ANIMAL_SELECTED_FIRST = "myFirstAnimal";
-        final String ANIMAL_SELECTED_SECOND = "mySecondAnimal";
-        final String OPERATOR_NAME = "大明";
+        File myFirstFile = new File("C:\\Users\\12909\\Desktop\\desktopFolder\\myFirstFile.txt");
+        File mySecondFile = new File("C:\\Users\\12909\\Desktop\\desktopFolder\\mySecondFile.dat");
 
-        Manager<String, IRun> manager = new Manager<>(new HashMap<String, IRun>());
-        Cat myCat = new Cat("xiaoXiong");
-        Dog myDog = new Dog("pePerPe");
+        myFirstFile.createNewFile();
+        mySecondFile.createNewFile();
+        //判断文件是否存在
 
-        manager.set(ANIMAL_SELECTED_FIRST, myCat);
-        manager.set(ANIMAL_SELECTED_SECOND, myDog);
+        if (myFirstFile.exists()) {
+            System.out.println("文件存在");
+        } else {
+            System.out.println("文件不存在");
+        }
 
-        manager.run(ANIMAL_SELECTED_FIRST);
-        manager.run(ANIMAL_SELECTED_SECOND);
-        //迭代器的使用测试：
+        //判断是否是文件夹
+        if (myFirstFile.isDirectory()) {
+            System.out.println("是文件夹");
+        } else {
+            System.out.println("不是文件夹");
+        }
 
-//        for (String key : manager.keySet()) {
-//            System.out.println(key);
+        if (myFirstFile.canRead() && myFirstFile.canWrite()) {
+            System.out.println("可以读写");
+        }
+
+//        //提示输入数据，并且以#结束
+//        System.out.println("请输入数据，以#结束");
+
+        //创建文件输入字节流
+//        try {
+//            FileOutputStream fileOutputStream = new FileOutputStream(myFirstFile);
+//            while (true) {
+//                int input;
+//                while ((input = System.in.read()) != '#') {
+//                    fileOutputStream.write(input);
+//                }
+//                System.out.println("写入完成。");
+//                break;
+//            }
+//        } catch (IOException e) {
+//            System.out.println(e.toString());
+//        }
+//        //读取文件里的内容
+//        System.out.println("################################################################################");
+//        try {
+//            FileInputStream fileInputStream = new FileInputStream(myFirstFile);
+//            while (fileInputStream.read(buffer) != -1) {
+//                System.out.println(new String(buffer));
+//            }
+//            System.out.println("读取完成");
+//            fileInputStream.close();
+//        }
+//        catch (IOException e) {
+//            System.out.println("读取文件是出错。");
+//        }
+//        try{
+//            DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(mySecondFile));
+//            dataOutputStream.writeInt(255);
+//            dataOutputStream.writeInt(-1);
+//            dataOutputStream.writeInt(0);
+//
+//            System.out.println("写入成功。");
+//        }
+//        catch (IOException e) {
+//            System.out.println("Problem reading" + myFirstFile.toString());
 //        }
 
+        try{
+            DataInputStream dataInputStream = new DataInputStream(new FileInputStream(mySecondFile));
+            sum += dataInputStream.readInt();
+            sum += dataInputStream.readInt();
+            sum += dataInputStream.readInt();
 
-        IRun testAnimal = manager.get(ANIMAL_SELECTED_FIRST);
-        IRun testAnimal2 = manager.get(ANIMAL_SELECTED_SECOND);
-
-        System.out.println(testAnimal);
-        System.out.println(testAnimal2);
-
+            System.out.println("sum = " + sum);
+        }
+        catch (IOException e) {
+            System.out.println("Problem reading" + myFirstFile.toString());
+        }
     }
 }
