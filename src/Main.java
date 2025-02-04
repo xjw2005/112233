89aea0ed31f8af3ad5d299bcfd22b6fd86439f07
熊健文 <1290922719@qq.com>
2025-02-05 00:13:49 +0800

    我的java补课8
    * BufferedFile类

--- src/Main.java
+++ src/Main.java
@@ -12,85 +12,45 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        byte[] buffer = new byte[1024];
-        int sum = 0;
+        String fileName = "C:\\Users\\12909\\Desktop\\desktopFolder\\testBufferedWriter.txt";
 
-        File myFirstFile = new File("C:\\Users\\12909\\Desktop\\desktopFolder\\myFirstFile.txt");
-        File mySecondFile = new File("C:\\Users\\12909\\Desktop\\desktopFolder\\mySecondFile.dat");
 
-        myFirstFile.createNewFile();
-        mySecondFile.createNewFile();
-        //判断文件是否存在
-
-        if (myFirstFile.exists()) {
-            System.out.println("文件存在");
-        } else {
-            System.out.println("文件不存在");
-        }
-
-        //判断是否是文件夹
-        if (myFirstFile.isDirectory()) {
-            System.out.println("是文件夹");
-        } else {
-            System.out.println("不是文件夹");
-        }
-
-        if (myFirstFile.canRead() && myFirstFile.canWrite()) {
-            System.out.println("可以读写");
-        }
-
-//        //提示输入数据，并且以#结束
-//        System.out.println("请输入数据，以#结束");
-
-        //创建文件输入字节流
 //        try {
-//            FileOutputStream fileOutputStream = new FileOutputStream(myFirstFile);
-//            while (true) {
-//                int input;
-//                while ((input = System.in.read()) != '#') {
-//                    fileOutputStream.write(input);
-//                }
-//                System.out.println("写入完成。");
-//                break;
-//            }
-//        } catch (IOException e) {
-//            System.out.println(e.toString());
+//            FileWriter writerMan = new FileWriter(fileName, false);
+//            writerMan.write("This is a test.");
+//            System.out.println("Successfully wrote to the file.");
+//            writerMan.close();
 //        }
-//        //读取文件里的内容
-//        System.out.println("################################################################################");
+//        catch (IOException e){
+//            System.out.println("Problem writing " + fileName);
+//        }
+
+        //BufferedWriter测试
 //        try {
-//            FileInputStream fileInputStream = new FileInputStream(myFirstFile);
-//            while (fileInputStream.read(buffer) != -1) {
-//                System.out.println(new String(buffer));
-//            }
-//            System.out.println("读取完成");
-//            fileInputStream.close();
+//            BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(fileName, false));
+//            bufferWriter.write("This is the second test. ho~wa!!!");
+//            bufferWriter.newLine();
+//            bufferWriter.write("Today is a lovely day again!");
+//            bufferWriter.close();
+//            System.out.println("Successfully wrote to the file.");
 //        }
-//        catch (IOException e) {
-//            System.out.println("读取文件是出错。");
-//        }
-//        try{
-//            DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(mySecondFile));
-//            dataOutputStream.writeInt(255);
-//            dataOutputStream.writeInt(-1);
-//            dataOutputStream.writeInt(0);
-//
-//            System.out.println("写入成功。");
-//        }
-//        catch (IOException e) {
-//            System.out.println("Problem reading" + myFirstFile.toString());
+//        catch (IOException e){
+//            System.out.println("Problem writing " + fileName);
 //        }
 
-        try{
-            DataInputStream dataInputStream = new DataInputStream(new FileInputStream(mySecondFile));
-            sum += dataInputStream.readInt();
-            sum += dataInputStream.readInt();
-            sum += dataInputStream.readInt();
+        //测试BufferedReader
 
-            System.out.println("sum = " + sum);
+        try {
+            BufferedReader bufferReader = new BufferedReader(new FileReader(fileName));
+            String line = bufferReader.readLine();
+
+            while (line != null){
+                System.out.println(line);
+                line = bufferReader.readLine();
+            }
         }
-        catch (IOException e) {
-            System.out.println("Problem reading" + myFirstFile.toString());
+        catch (IOException e){
+            System.out.println("Problem reading " + fileName);
         }
     }
 }
