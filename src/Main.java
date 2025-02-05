522a82f7126ab060bd09b7c0648435ebc38b26b1
熊健文 <1290922719@qq.com>
2025-02-05 20:34:33 +0800

    我的java补课10
    * Scanner类的日期解析

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
-import Learn.Cat;
-import Learn.Dog;
-import Learn.IRun;
-import Learn.Manager;
-
 import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Scanner;
 
 
@@ -17,15 +9,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        Scanner scanner = new Scanner(System.in);
 
         //Scanner 测试：
 
         System.out.println("请输入日期（格式：yyyy-MM-dd）：");
         String inputDate = scanner.nextLine();
-//        System.out.println("请输入数据：");
-//        String str = scanner.next();
-//        System.out.println("输入的数据为：" + str);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         DateTimeFormatter outPutFormatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
